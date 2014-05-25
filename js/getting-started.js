faaf6ea4c6e70468672ad4ee1346a4cddb471b4c
Eason Chen <easoncxz@gmail.com>
2014-05-25 15:58:38 +1200

    Applied the js from the getting-started page onto the climates page and fixed it quicked.

--- js/getting-started.js
+++ js/getting-started.js
@@ -2,12 +2,10 @@
     $('.option').on('click', function(){
         var pc = $(this).parents('.card');
         if (!pc.is('.off')) {
-            $(this).toggleClass('on');
             pc.toggleClass('off');
+            $(this).toggleClass('on');
             var n = $(this).parents('.card').next();
-            if (n.is('.card')) {
-                n.toggleClass('off');
-            }
+            n.toggleClass('off');
             $('body').animate({
                 scrollTop: n.offset().top
             }, 1000);
